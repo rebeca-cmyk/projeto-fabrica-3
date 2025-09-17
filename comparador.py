293a6d7c12646775573c2624b7291b433c977a55
Rebeca <rebecamascarenhasalmeidaruas@gmail>
2025-09-17 15:09:45 -0300

    atualizaçao

--- comparador.py
+++ comparador.py
@@ -1,9 +1,9 @@
 produto = input("produto: ")
-nome_do_mercado = input("nome do mercado: ")
+nome_mercado = input("nome do mercado: ")
 preco_produto1 = float(input("preco do produto: "))
-nome_do_mercado2 = input("nome do mercado2: ")
+nome_mercado2 = input("nome do mercado2: ")
 preco_produto2 = float(input("preco do produto: "))
-nome_do_mercado3 = input("nome do mercado: ")
+nome_mercado3 = input("nome do mercado: ")
 preco_do_produto3 = float(input("preco do produto: "))
 
 if preco_produto1 < preco_produto2 and preco_produto1<preco_do_produto3:
