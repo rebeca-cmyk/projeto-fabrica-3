0a1de50e686fa628f92a2debaec321605a7f4b17
Rebeca <rebecamascarenhasalmeidaruas@gmail>
2025-09-17 15:14:01 -0300

    atualizaçao

--- comparador.py
+++ comparador.py
@@ -6,9 +6,9 @@ preco_produto2 = float(input("preco do produto: "))
 nome_mercado3 = input("nome do mercado: ")
 preco_produto3 = float(input("preco do produto: "))
 
-if preco_produto1 < preco_produto2 and preco_produto1<preco_do_produto3:
+if preco_produto1 < preco_produto2 and preco_produto1<preco_produto3:
     print(f"{produto}, {nome_mercado1}, {preco_produto1}")
-elif preco_produto2<preco_produto1 and preco_produto2<preco_do_produto3:
+elif preco_produto2<preco_produto1 and preco_produto2<preco_produto3:
     print(f"{produto}, {nome_mercado2}, {preco_produto2}")
 else:
     print(f"{produto}, {nome_mercado3}, {preco_produto3}")
