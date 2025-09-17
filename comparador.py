cb0ba95c8bcd77f04b89a9a9a191fdd99c658a0f
Rebeca <rebecamascarenhasalmeidaruas@gmail>
2025-09-17 15:02:45 -0300

    criar funçao comparadora

---
+++ comparador.py
@@ -0,0 +1,14 @@
+produto = input("produto: ")
+nome_do_mercado = input("nome do mercado: ")
+preco_produto1 = float(input("preco do produto: "))
+nome_do_mercado2 = input("nome do mercado2: ")
+preco_produto2 = float(input("preco do produto: "))
+nome_do_mercado3 = input("nome do mercado: ")
+preco_do_produto3 = float(input("preco do produto: "))
+
+if preco_produto1 < preco_produto2 and preco_produto1<preco_do_produto3:
+    print(f"{produto}, {nome_mercado1}, {preco_produto1}")
+elif preco_produto2<preco_produto1 and preco_produto2<preco_do_produto3:
+    print(f"{produto}, {nome_mercado2}, {preco_produto2}")
+else:
+    print(f"{produto}, {nome_mercado3}, {preco_produto3}")
